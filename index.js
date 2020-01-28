a1c0a06d6cdc74113d338fa807833e023e5038f3
Dylan Collins <dcollins5590@students.mc3.edu>
2020-01-28 17:21:53 -0500

    completed 16 questions

--- index.js
+++ index.js
@@ -71,8 +71,15 @@ Person.prototype.toString = function(item){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
+}
+Car.prototype.fill = function (gallons){
+  this.tank += gallons
+  return this.tank
 }
 
 /*
